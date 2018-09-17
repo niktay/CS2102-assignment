69c11610d6906f17a2878344ce3302d73ae7a2a9
Glenice Tan <blackroxs@users.noreply.github.com>
2018-09-17 17:30:45 +0800

    Remove bugs in init.sql (#9)
    * Fix bug in database fixture

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Account
 (
  account_id SERIAL PRIMARY KEY,
- first_name VARCHAR(64) NOT NULL ,
- last_name  VARCHAR(64) NOT NULL ,
- is_admin   BOOLEAN NOT NULL ,
- email      VARCHAR(64) NOT NULL ,
+ first_name VARCHAR(64) NOT NULL,
+ last_name VARCHAR(64) NOT NULL,
+ is_admin BOOLEAN NOT NULL,
+ email VARCHAR(64) NOT NULL
 );
 
-INSERT INTO Account VALUES ('XIE XIN', 'CHEN', 0, 'xiexin2011@gmail.com');
-INSERT INTO Account VALUES ('MARY', 'TAN', 0, 'mt1993@gmail.com');
-INSERT INTO Account VALUES ('SUSASSANE', 'LIM', 0, 'suzzlim99@gmail.com');
-INSERT INTO Account VALUES ('FABIAN', 'KOH', 0, 'fabiancool@gmail.com');
-INSERT INTO Account VALUES ('JASMINE', 'JEE', 1, 'jasjee91@gmail.com');
+INSERT INTO Account VALUES (DEFAULT, 'XIE XIN', 'CHEN', false, 'xiexin2011@gmail.com');
+INSERT INTO Account VALUES (DEFAULT, 'MARY', 'TAN', false, 'mt1993@gmail.com');
+INSERT INTO Account VALUES (DEFAULT, 'SUSASSANE', 'LIM', false, 'suzzlim99@gmail.com');
+INSERT INTO Account VALUES (DEFAULT, 'FABIAN', 'KOH', false, 'fabiancool@gmail.com');
+INSERT INTO Account VALUES (DEFAULT, 'JASMINE', 'JEE', true, 'jasjee91@gmail.com');
