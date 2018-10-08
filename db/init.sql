0d93bc8842d552c5905a9a5a2fda611eb00bc0b9
Nikolas Tay <niktay94@gmail.com>
2018-10-08 13:00:25 +0800

    Update database fixture

--- db/init.sql
+++ db/init.sql
@@ -13,21 +13,21 @@ CONSTRAINT username_not_empty CHECK(LENGTH(username) > 0)
 
 CREATE TABLE driver
 (
-license_num VARCHAR(64) NOT NULL,
+license_number VARCHAR(64) NOT NULL,
 username VARCHAR(64) NOT NULL UNIQUE,
 driving_since DATE NOT NULL,
-PRIMARY KEY (license_num),
+PRIMARY KEY (license_number),
 FOREIGN KEY (username) REFERENCES account (username)
 );
 
 CREATE TABLE advertisement
 (
 start_timestamp TIMESTAMP NOT NULL,
-license_num VARCHAR(64) NOT NULL,
+license_number VARCHAR(64) NOT NULL,
 origin VARCHAR(255) NOT NULL,
 destination VARCHAR(255) NOT NULL,
-PRIMARY KEY (start_timestamp, license_num),
-FOREIGN KEY (license_num) REFERENCES driver (license_num),
+PRIMARY KEY (start_timestamp, license_number),
+FOREIGN KEY (license_number) REFERENCES driver (license_number),
 CONSTRAINT origin_destination_different CHECK (origin != destination)
 );
 
@@ -36,9 +36,9 @@ CREATE TABLE car
 license_plate VARCHAR(10) NOT NULL,
 brand VARCHAR(64) NOT NULL,
 model VARCHAR(64) NOT NULL,
-license_num VARCHAR(64) NOT NULL,
+license_number VARCHAR(64) NOT NULL,
 PRIMARY KEY (license_plate),
-FOREIGN KEY (license_num) REFERENCES driver (license_num)
+FOREIGN KEY (license_number) REFERENCES driver (license_number)
 );
 
 CREATE TABLE bid
@@ -47,18 +47,18 @@ bid_id INTEGER NOT NULL,
 price DECIMAL(19,4) NOT NULL,
 username VARCHAR(64) NOT NULL,
 start_timestamp TIMESTAMP NOT NULL,
-license_num VARCHAR(64) NOT NULL,
+license_number VARCHAR(64) NOT NULL,
 PRIMARY KEY (bid_id),
 FOREIGN KEY (username) REFERENCES account (username),
-FOREIGN KEY (start_timestamp, license_num) REFERENCES advertisement
-(start_timestamp, license_num),
+FOREIGN KEY (start_timestamp, license_number) REFERENCES advertisement
+(start_timestamp, license_number),
 CONSTRAINT price_not_negative CHECK (price > 0)
 );
 
 CREATE TABLE ride
 (
 bid_id INTEGER NOT NULL,
-won_timestamp TIMESTAMP NOT NULL,
-PRIMARY KEY (bid_id, won_timestamp),
+confirmed_timestamp TIMESTAMP NOT NULL,
+PRIMARY KEY (bid_id, confirmed_timestamp),
 FOREIGN KEY (bid_id) REFERENCES bid (bid_id)
 );
